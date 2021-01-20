e853f1133a72a0b8d915c022273dc66d6f47389e
EpsteinRoss-FS <repstein@student.fullsail.edu>
2021-01-19 20:38:10 -0500

    Initial commit - added animal class, ITrainable interace, and program class

---
+++ Epstein_Ross_Interfaces/ITrainable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Epstein_Ross_Interfaces
+{
+    interface ITrainable
+    {
+        Dictionary<string, string> Behaviors { get; set; }
+        string Perform(string signal);
+        string Train(string signal, string behavior);
+    }
+}
